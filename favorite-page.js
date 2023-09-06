a8754bed012a2538462bb7d2ddde0be8a5d8eede
Fmquito45 <fmquitox45@gmail.com>
2023-09-06 11:13:52 -0400

    creates JS file for favorites page

---
+++ favorite-page.js
@@ -0,0 +1,33 @@
+//DEPENDENCIES ====================
+var apiKeyTmbd = "76c745d0d38df70f6fb5ec449119b744";
+var apiKeyOmbd = "3c12800d";
+
+//DATA=============================
+
+//FUNCTIONS =======================
+//fetch request TMBD
+function getTmbdData() {
+  var queryURL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKeyTmbd}`; // use /tv
+  fetch(queryURL).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
+}
+getTmbdData();
+// fetch request OMDB
+function getOmbdData() {
+  var queryURL = `http://www.omdbapi.com/?apikey=${apiKeyOmbd}&`;
+  fetch(queryURL).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
+}
+getOmbdData();
+
+//USER INTERACTIONS================
