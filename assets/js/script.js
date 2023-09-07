68237c7d61f3ba47b1317bdfd1506ed420f8e22c
Taylor <taysully94@gmail.com>
2023-09-07 12:57:38 -0400

    adds the slider data to the local storage, pulls the numbers on the slide, moves the slider to data section of JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,33 +3,6 @@ var apiKeyTmbd = "76c745d0d38df70f6fb5ec449119b744";
 var apiKeyOmbd = "3c12800d";
 
 //DATA=============================
-
-//FUNCTIONS =======================
-//fetch request TMBD
-function getTmbdData() {
-  var queryURL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKeyTmbd}`; // use /tv
-  fetch(queryURL).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-      });
-    }
-  });
-}
-getTmbdData();
-// fetch request OMDB
-function getOmbdData() {
-  var queryURL = `http://www.omdbapi.com/?apikey=${apiKeyOmbd}&`;
-  fetch(queryURL).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-      });
-    }
-  });
-}
-getOmbdData();
-
 // adds range slider
 var slider = document.getElementById("test-slider");
 
@@ -57,40 +30,59 @@ noUiSlider.create(slider, {
   //     decimals: 0,
   //   }),
 });
-function getYearRange() {
-  
-  console.log(slider.noUiSlider.get()); //returns an array 
 
+//FUNCTIONS =======================
+//fetch request TMBD
+function getTmbdData() {
+  var queryURL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKeyTmbd}`; // use /tv
+  fetch(queryURL).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
 }
-
-
+getTmbdData();
+// fetch request OMDB
+function getOmbdData() {
+  var queryURL = `http://www.omdbapi.com/?apikey=${apiKeyOmbd}&`;
+  fetch(queryURL).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
+}
+getOmbdData();
 
 //USER INTERACTIONS================
 // Handle form submission
 console.log($("#search-form"));
-$('#search-form').submit(function (event) {
+$("#search-form").submit(function (event) {
   event.preventDefault(); // Prevent the form from submitting normally
-  console.log("form clicked")
+  console.log("form clicked");
   // Get user input from the search input field
-  var searchQuery = $('#search').val();
+  var searchQuery = $("#search").val();
 
   // Get selected values from dropdowns
-  var genreValue = $('#genre-dropdown').val();
-  var yearRangeValue = $('#slider').val();
-  var durationValue = $('#duration-dropdown').val();
-  var typeValue = $('#type-dropdown').val();
+  var genreValue = $("#genre-dropdown").val();
+  var yearRangeValue = slider.noUiSlider.get();
+  var durationValue = $("#duration-dropdown").val();
+  var typeValue = $("#type-dropdown").val();
 
   // Create an object to store the user's preferences
   var userPreferences = {
-      searchQuery: searchQuery,
-      genre: genreValue,
-      yearRange: yearRangeValue,
-      duration: durationValue,
-      type: typeValue
+    searchQuery: searchQuery,
+    genre: genreValue,
+    yearRange: yearRangeValue,
+    duration: durationValue,
+    type: typeValue,
   };
 
   // Store the user's preferences in localStorage
-  localStorage.setItem('userPreferences', JSON.stringify(userPreferences));
+  localStorage.setItem("userPreferences", JSON.stringify(userPreferences));
 
   // You can also retrieve data from localStorage later if needed
   // var storedPreferences = localStorage.getItem('userPreferences');
@@ -98,10 +90,10 @@ $('#search-form').submit(function (event) {
   // console.log(parsedPreferences);
 
   // For demonstration, we'll just display a confirmation message
-  alert('Your preferences have been saved locally.');
+  alert("Your preferences have been saved locally.");
 
   // Redirect to the results page
-  window.location.href="result-page.html";
+  window.location.href = "result-page.html";
 
   // You can further process the data or update the page content.
 });
